82ed585773437fd679260a51a3b07d60778a2145
Juan Pablo @codigoconjuan <blackmaxxgdl18@hotmail.com>
2021-11-22 16:33:10 -0600

    ternarios

--- js/17.js
+++ js/17.js
@@ -1,12 +1,20 @@
-// Scope de Variables
-const precio = 300;
+// Ternarios
+// Un operador ternario básicamente es un IF en una sola linea
 
-function unaFuncion() {
-    const precio = 400;
-    console.log(precio)
-}
-unaFuncion()
+const autenticado = false;
+autenticado ? console.log('Usuario Autenticado') : console.log('no autenticado')
 
-if( true ) {
-    console.log(precio)
-}
+
+// Doble Ternario
+
+const saldo = 300
+const pagar = 400
+const tarjeta = true
+saldo > pagar ? console.log('Puedes pagar con saldo') : tarjeta ? console.log('puedes pagar con tarjeta') : console.log('no puedes pagar')
+
+
+// Operador && - Ideal cuando solo hay una condición...
+
+const admin = true
+admin && console.log('Eres Admin')
+
