de1d50820de8a156cf92607e3ebb3f09c15f66e0
Juan Pablo De la torre <blackmaxxgdl18@hotmail.com>
2021-11-22 17:45:57 -0600

    Fin de Inputs

--- js/21.js
+++ js/21.js
@@ -1,7 +1,15 @@
 // Eventos del DOM (Inputs)
 
 const inputNombre = document.querySelector('.nombre');
+const inputPassword = document.querySelector('.password');
 
-inputNombre.addEventListener('input', function() { //keydown, keyup o input
-    console.log('escribiendo')
+inputNombre.addEventListener('input', function(e) { //keydown, keyup o input
+    console.log(e.target.value)
+})
+
+inputPassword.addEventListener('input', function(e) { //keydown, keyup o input
+    inputPassword.type = 'text'
+    setTimeout(() => {
+        inputPassword.type = 'password'
+    }, 1000);
 })
