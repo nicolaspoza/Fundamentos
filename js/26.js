c439630b149128b2b44a49011748ac337db9c843
Juan Pablo @codigoconjuan <blackmaxxgdl18@hotmail.com>
2021-11-22 23:18:38 -0600

    ejemplo async await

--- js/26.js
+++ js/26.js
@@ -1,1 +1,23 @@
 // Async Await 
+
+const url = "http://localhost:3000/productos";
+
+fetch(url)
+    .then( respuesta => respuesta.json())
+    .then( resultado => {
+        console.log(resultado[0])
+        resultado.forEach(producto =>  console.log(producto))
+    })
+
+async function consultarAPI() {
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    console.log(resultado)
+}
+
+const consultarAPI = async () => {
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    console.log(resultado)
+}
+consultarAPI();
