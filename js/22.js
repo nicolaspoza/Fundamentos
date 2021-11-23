378c9b6fdf19886bdce7c1639a06b7e146302193
Juan Pablo De la torre <blackmaxxgdl18@hotmail.com>
2021-11-22 18:05:07 -0600

    formularios finalizado

--- js/22.js
+++ js/22.js
@@ -1,1 +1,14 @@
 // Eventos del DOM (Enviar Formulario)
+const formulario = document.querySelector('#formulario')
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nombre = document.querySelector('.nombre').value
+    const password = document.querySelector('.password').value
+
+    if(nombre === '' || password === '') {
+        console.log('Ambos campos son obligatorios')
+    } else {
+        console.log('Todo bien')
+    }
+})
